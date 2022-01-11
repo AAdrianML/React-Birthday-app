b7cc1cb7920d00efe8593c311108b0227b13728c
AAdrianML <trabajo.aaml@gmail.com>
2022-01-11 16:16:50 -0700

    h2 ->h1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h1>reminder project setup</h1>;
 }
 
 export default App;
